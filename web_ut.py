c5927e0981450609a7412f36bd16add2b29f70cb
wjsaveve@163.com <wjsaveve@163.com>
2021-04-19 16:41:28 +0800

    改回原写法

--- web_ut.py
+++ web_ut.py
@@ -17,9 +17,9 @@ class ISelenium(unittest.TestCase):
     def get_config(self):
         config = configparser.ConfigParser()
         # windows写法
-        config.read(os.path.join('C:', os.environ['HOMEPATH'], 'iselenium.ini'))
+        # config.read(os.path.join('C:', os.environ['HOMEPATH'], 'iselenium.ini'))
         # 在liunux中是这么写
-        # config.read(os.path.join(os.environ['HOME'], 'iselenium.ini'))
+        config.read(os.path.join(os.environ['HOME'], 'iselenium.ini'))
         return config
 
     def tearDown(self):
